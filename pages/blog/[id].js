3c86a305f71ae75d0850a1af0b0535add0983bd5
Name <kumamoto.yusuke.hobby@gmail.com>
2025-02-15 07:37:56 +0900

    文言微調整

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,58 +1,44 @@
 import Head from "next/head";
 import { client } from "../../libs/client";
 import Header from '../../components/header';
-import Prism from 'prismjs'; 
-import 'prismjs/themes/prism-dark.css'; 
-import 'prismjs/components/prism-javascript'; 
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-dark.css';
+import 'prismjs/components/prism-javascript';
 import { useEffect } from 'react';
-import $ from 'jquery'
-export default function BlogId({ blog,category }) {
+export default function BlogId({ blog, category }) {
   useEffect(() => {
-    console.log(category)
-    console.log(blog.category)
-    $('.kiji').each(function(){
-      $(this).html($(this).html().replace(/\n/g,"<br>"))
-  })
-  // Prism.jsを実行
-  Prism.highlightAll()
-   }, []);
-  
+    // Prism.jsを実行
+    Prism.highlightAll()
+  }, []);
+
   return (
     <>
-    <Head>
-    <title>{blog.title}</title>
-    <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Header/>
-    <main id="blog">
-    <div className="contents_wrap">
-      <div className="main_content">
+      <Head>
+        <title>{blog.title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main id="blog">
+        <div className="contents_wrap">
+          <div className="main_content">
 
-        <h1 className="title" dangerouslySetInnerHTML={{ __html: blog.title }}></h1>
+            <h1 className="title" dangerouslySetInnerHTML={{ __html: blog.title }}></h1>
 
-        <div className="content">
-          <div className="category_area">
-            <ul className="category_ul">
-              {blog.category.map((category) => (
-                <li className="category_list" key={category.id}>
-                  {category.name}
-                </li>
-              ))}
-            </ul>
+            <div className="content">
+              <div className="category_area">
+                <ul className="category_ul">
+                  {blog.category.map((category) => (
+                    <li className="category_list" key={category.id}>
+                      {category.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="blog_area" dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+            </div>
           </div>
-          <div className="blog_area" dangerouslySetInnerHTML={{ __html: blog.content }}></div>
         </div>
-      </div>
-      <div className="side_content">
-      <div className="content">
-      </div>
-      <div className="content">
-
-      </div>
-      <div className="content"></div>
-      </div>
-    </div>
-    </main>
+      </main>
     </>
   );
 }
@@ -69,7 +55,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const blogData = await client.get({ endpoint: "blog", contentId: id });
-  const categoryData = await client.get({ endpoint: "categories"});
+  const categoryData = await client.get({ endpoint: "categories" });
   return {
     props: {
       blog: blogData,
