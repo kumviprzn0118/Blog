57615d5a32b2cf6b604ebaf26d5d0273b2dfc0b3
Name <kumamoto.yusuke.hobby@gmail.com>
2025-02-16 19:08:46 +0900

    ページネーション追加

--- pages/blog/list.js
+++ pages/blog/list.js
@@ -3,19 +3,28 @@ import Header from '../../components/header';
 import Link from 'next/link';
 import styles from "../../styles/BlogList.module.css";
 import $ from 'jquery'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { client } from "../../libs/client";
-import Script from 'next/script'
+import Script from 'next/script';
 export default function Home({ blog }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
   useEffect(() => {
     $('.blog_publish_time').each(function () {
       $(this).html($(this).html().slice(0, 10));
     })
-    $('.publishedat').each(function(){
+    $('.publishedat').each(function () {
       let txt = String($(this).text().match(/([0-9]{4}-[0-9]{2}-[0-9]{2})/)).split(',')[0]
       $(this).text(txt)
     })
   }, []);
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = blog.slice(indexOfFirstPost, indexOfLastPost);
+
+  const totalPages = Math.ceil(blog.length / postsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div>
       <Head>
@@ -29,14 +38,14 @@ export default function Home({ blog }) {
             <div className="content">
               <h1>記事一覧</h1>
               <ul className={styles.blog_ul}>
-                {blog.map((blog) => (
+                {currentPosts.map((blog) => (
                   <li className={styles.blog_list} key={blog.id}>
                     <Link className={styles.link} href={`/blog/${blog.id}`}>
-                    <div className={styles.img_wrapper}>
-                    {blog.image && blog.image.url && (
-                        <img className={styles.img} src={blog.image.url} alt={blog.title} />
-                      )}
-                    </div>
+                      <div className={styles.img_wrapper}>
+                        {blog.image && blog.image.url && (
+                          <img className={styles.img} src={blog.image.url} alt={blog.title} />
+                        )}
+                      </div>
 
                       <div className={styles.wrap_info}>
                         <div className={styles.blog_title}>{blog.title}</div>
@@ -46,6 +55,26 @@ export default function Home({ blog }) {
                   </li>
                 ))}
               </ul>
+            </div>
+            {/* ページネーション */}
+            <div className={styles.pagination}>
+              {currentPage > 1 && (
+                <button onClick={() => paginate(currentPage - 1)}>前へ</button>
+              )}
+
+              {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+                <button
+                  key={pageNumber}
+                  onClick={() => paginate(pageNumber)}
+                  className={currentPage === pageNumber ? styles.active : ''}
+                >
+                  {pageNumber}
+                </button>
+              ))}
+
+              {currentPage < totalPages && (
+                <button onClick={() => paginate(currentPage + 1)}>次へ</button>
+              )}
             </div>
 
 
